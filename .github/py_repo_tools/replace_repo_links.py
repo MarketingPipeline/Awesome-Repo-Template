1d4465507acd24146c519beeb7218790d1cde8bc
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-04 20:48:35 -0400

    Create replace_repo_links.py

---
+++ .github/py_repo_tools/replace_repo_links.py
@@ -0,0 +1,15 @@
+# Define the filename here you want to replace content in
+FileName = "README.md"
+
+Text_To_Replace = ""
+
+Text_To_Replace_With = ""
+
+
+with open(FileName, 'r') as f:
+    contents = f.read()
+    # Define the first line where your content will be replaced / added 
+    contents = contents.replace('Media-Element.js/media-element.min.js', 'version/1.0.1/src/media-elements.js')
+    
+with open(FileName, 'w') as f:
+    f.write(contents)  
